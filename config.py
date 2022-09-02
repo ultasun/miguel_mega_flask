8b9571ac95e2426528a278c68f0f2d55eeb8c249
ultasun <ultasun@icloud.com>
2022-09-02 17:23:50 -0400

    completed chapter 9.

--- config.py
+++ config.py
@@ -3,10 +3,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # CSRF protection key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'good-luck-guessing-it'
+
+    # database options
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # application options (or "knobs")
+    POSTS_PER_PAGE = 3
+
+    # email logging options
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
