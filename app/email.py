8b6b41bb2fed572c71e5af7eac74750ca780f8aa
ultasun <ultasun@icloud.com>
2022-09-03 23:39:41 -0400

    completed chapter 10.

--- app/email.py
+++ app/email.py
@@ -1,12 +1,22 @@
 from flask import render_template
 from flask_mail import Message
 from app import app, mail
+from threading import Thread
+
+# regarding 'with' statements in this...context:
+# https://docs.python.org/3/reference/compound_stmts.html#with
+# TODO understand how 'with' is used in more detail
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    #mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
